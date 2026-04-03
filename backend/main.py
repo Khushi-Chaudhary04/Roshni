29d23a1353dede8644c428385e9b6e4de1471402
Khushi Chaudhary <145854833+Khushi-Chaudhary04@users.noreply.github.com>
2026-04-03 22:48:12 +0530

    Add localhost to CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -48,7 +48,10 @@ app = FastAPI(
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://roshni-frontend.onrender.com/"],
+    allow_origins=[
+    "https://roshni-frontend.onrender.com",
+    "http://localhost:5173"
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
